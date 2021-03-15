a0587b10a4697427902c558df895190e2d946bbb
mery <merryoff@gmail.com>
2021-03-15 22:29:44 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const a = 'hundred';
+    let result = '';
+    if (number == 0) {
+        return 'zero';
+    }
+    if (number > 99) { result += `${digits[Math.floor(number / 100)]} ${a}` };
+    if (number % 100 > 19) {
+        result += ` ${tens[Math.floor(number % 100 / 10) - 2]}`
+        if (number % 10 !== 0) {
+            result += ` ${digits[Math.floor(number % 10)]}`;
+        }
+    }
+    if (number % 100 <= 19 && number % 100 > 0) result += ` ${digits[Math.floor(number % 100)]}`;
+    if ((number + '').length < 3) result = result.slice(1);
+    return result;
 }
